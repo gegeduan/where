c86711bfb44f5c3994ffc33b74d36fae921acfdd
xiaobuding <1942304871@qq.com>
2020-04-07 21:06:17 +0800

    项目目录初始化,和要使用插件和库的准备

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './styles/reset.css'
+import './styles/border.css'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    我是首页
+    <router-link to='/List'>跳转</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style  scoped>
+</style>
