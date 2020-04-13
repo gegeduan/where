bec0d7f9c5664f94a4353f60ed9604b35b5ebc2d
xiaobuding <1942304871@qq.com>
2020-04-13 17:54:58 +0800

    search 组件的搜索功能的实现[C

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="city">
       <city-header></city-header>
-      <city-search></city-search>
+      <city-search :cities='cities'></city-search>
       <city-list :listCities='cities' :listHotCities='hotCities' :ListLetter='letter'></city-list>
       <city-alphabet @change='handleLetterChange' :listCities='cities'></city-alphabet>
   </div>
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,22 +1,78 @@
 <template>
   <div class="search">
-     <input  class='search-input' type='text' placeholder='输入城市或者拼音'>
+     <input class='search-input'
+      type='text'
+      v-model="keyword"
+      placeholder='输入城市或者拼音'>
+     <div class='search-content' ref='search' v-show='keyword'>
+         <div>
+            <div class='item-content border-bottom'
+            v-for='item of list'
+            :key='item.id'
+            >{{item.name}}</div>
+            <div class='item-content border-bottom' v-show='hasNoData '>没有找到匹配项</div>
+         </div>
+     </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
   name: 'CitySearch',
+  props: {
+    cities: Object
+  },
   data () {
     return {
+      keyword: '',
+      list: [],
+      timer: null
     }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='stylus' scoped>
 @import '~@/styles/varibles.styl'
+.border-topbottom
+   &:before
+     border-color:#777
+   &:after
+     border-color:#777
+.border-bottom
+   &:before
+     border-color:#777
 .search
   height:.72rem
   background:$bgColor
@@ -29,4 +85,18 @@ export default {
     color:#666
     text-align:center
     border-radius:.06rem
+  .search-content
+    z-index:100
+    overflow:hidden
+    position:absolute
+    top:1.58rem
+    left:0
+    right:0
+    bottom:0
+    background:#eee
+    .item-content
+      color:#666
+      padding-left:.2rem
+      line-height:.62rem
+      background:#fff
 </style>
